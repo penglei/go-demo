86fa264bdd222018f3682ae7fe5ca01c2f4485ec
penglei <penglei@ybyte.org>
2018-12-03 14:30:06 +0800

    update script

--- hub-go-builder/local-build.sh
+++ hub-go-builder/local-build.sh
@@ -3,10 +3,9 @@
 export SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 export PROJECTDIR=$(cd $SCRIPTDIR/.. && pwd)
 
-cd "$SCRIPTDIR"
-
 image_tag=hub.tencentyun.com/workshop/go-demo-cache-builder:latest
+project_container_dir=/project
 
-docker build --build-arg REPO_DIR=/project -t $image_tag "$PROJECTDIR"
+docker build --build-arg REPO_DIR=$project_container_dir -t $image_tag -f $SCRIPTDIR/Dockerfile "$PROJECTDIR"
 #docker push $image_tag
 
