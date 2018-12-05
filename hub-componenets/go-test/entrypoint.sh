bc9578abc732172eb5a17002bea25a75cdaba5d4
penglei <penglei@ybyte.org>
2018-12-05 13:24:12 +0800

    run mysql in runit

--- hub-componenets/go-test/entrypoint.sh
+++ hub-componenets/go-test/entrypoint.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-export SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+set -e
+
+export SCRIPTDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 source $SCRIPTDIR/component-base/libs.sh
 
-init_mysql() {
-	source $SCRIPTDIR/mysql-entrypoint.sh
+wait_service() {
+	echo "Wait MySQL..."
+	for i in `seq 1 30`;
+	do
+		nc -z localhost 3306 && echo Success && exit 0
+		echo -n .
+		sleep 1
+	done
+	echo Failed waiting for MySQL && exit 1
+
 }
 
 do_task() {
-	init_mysql()
+	runsvdir /etc/sv > /dev/null 2>&1 &
+
+	(wait_service)
+
 	hub_git_clone
 
 	#TODO vgo test
