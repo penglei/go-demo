1ad3990a67c88f57193ca93555f1aa0910a95cdd
penglei <penglei@ybyte.org>
2018-12-07 13:28:47 +0800

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 #FROM golang:1.11.2-stretch as builder
 FROM hub.tencentyun.com/workshop/go-builder:latest as builder
-WORKDIR /go/src/github.com/qcloud2018/go-demo
-COPY . /go/src/github.com/qcloud2018/go-demo
-RUN vgo build -v -a -o /go-demo
+ARG GIT_REPO_DIR=/go/src/github.com/qcloud2018/go-demo
+COPY . $GIT_REPO_DIR
+WORKDIR $GIT_REPO_DIR
+RUN cd $GIT_REPO_DIR && vgo build -v -a -o /go-demo ./cmd/*.go
 
 FROM debian:stretch
 EXPOSE 8080
--- hub-components/go-builder/local-build.sh
+++ hub-components/go-builder/local-build.sh
@@ -3,9 +3,9 @@
 export SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 export PROJECTDIR=$(cd $SCRIPTDIR/../.. && pwd)
 
-image_tag=hub.tencentyun.com/workshop/go-demo-cache-builder:latest
+image=hub.tencentyun.com/workshop/go-demo-cache-builder:latest
 project_container_dir=/project
 
-docker build --build-arg REPO_DIR=$project_container_dir -t $image_tag -f $SCRIPTDIR/Dockerfile "$PROJECTDIR"
-#docker push $image_tag
+docker build --build-arg REPO_DIR=$project_container_dir -t $image -f $SCRIPTDIR/Dockerfile "$PROJECTDIR"
+#docker push $image
 
